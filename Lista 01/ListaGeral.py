9c59815e797b9581fc9164ba8258750e25313b19
paulinnleao <phleaoliveira@gmail.com>
2024-03-05 10:55:05 -0300

    Questão 4 finalizada

--- Lista 01/ListaGeral.py
+++ Lista 01/ListaGeral.py
@@ -3,3 +3,4 @@ import QuestoesService
 QuestoesService.Questao01.resolucao(5,5)
 QuestoesService.Questao02.resolucao([3, 2], [6, 8])
 QuestoesService.Questao03.resolucao([3, 2], [6, 8])
+QuestoesService.Questao04.resolucao([3, 2], [6, 8], [7, 3])
--- Lista 01/QuestoesService.py
+++ Lista 01/QuestoesService.py
@@ -1,15 +1,12 @@
 import matplotlib.pyplot as plot
 
-def calcularMaxEMin(ponto_a: list, ponto_b: list):
+def calcularMaxEMin(listaPontos: list):
     max: float = 5
     min: float = -5
-    for value in ponto_a:
-        max = (value + 2) if value >= max else max
-        min = (value - 2) if value <= min else min
-
-    for value in ponto_b:
-        max = (value + 2) if value >= max else max
-        min = (value - 2) if value <= min else min
+    for lista in listaPontos:
+        for value in lista:
+            max = (value + 2) if value >= max else max
+            min = (value - 2) if value <= min else min
 
     return [min, max]
 
@@ -24,7 +21,7 @@ class Questao01:
 
 class Questao02:
     def resolucao(ponto_a: list, ponto_b: list):
-        escala: list = calcularMaxEMin(ponto_a, ponto_b)
+        escala: list = calcularMaxEMin([ponto_a, ponto_b])
         figura, eixo = plot.subplots()
 
         eixo.set_xlim(escala[0], escala[1])
@@ -38,7 +35,7 @@ class Questao02:
 
 class Questao03:
     def resolucao(ponto_a: list, ponto_b: list):
-        escala: list = calcularMaxEMin(ponto_a, ponto_b)
+        escala: list = calcularMaxEMin([ponto_a, ponto_b])
         figura, eixo = plot.subplots()
 
         eixo.set_xlim(escala[0], escala[1])
@@ -51,3 +48,18 @@ class Questao03:
         eixo.plot([ponto_a[0], ponto_b[0]], [ponto_a[1], ponto_b[1]], 'k-', label='Linha AB')
         plot.show()
 
+class Questao04:
+    def resolucao(ponto_a: list, ponto_b: list, ponto_c: list):
+        escala: list = calcularMaxEMin([ponto_a, ponto_b, ponto_c])
+        figura, eixo = plot.subplots()
+
+        eixo.set_xlim(escala[0], escala[1])
+        eixo.set_ylim(escala[0], escala[1])
+        eixo.grid()
+
+        eixo.plot(ponto_a[0], ponto_a[1], 'bo', label = 'Ponto A')
+        eixo.plot(ponto_b[0], ponto_b[1], 'ro', label = 'Ponto B')
+        eixo.plot(ponto_c[0], ponto_c[1], 'go', label = 'Ponto C')
+
+        eixo.plot([ponto_a[0], ponto_b[0], ponto_c[0], ponto_a[0]], [ponto_a[1], ponto_b[1], ponto_c[1], ponto_a[1]], 'k-', label='Triangulo ABC')
+        plot.show()
